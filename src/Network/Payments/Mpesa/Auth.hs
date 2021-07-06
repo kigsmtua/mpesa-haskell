88a39d2cfc7ba7b98f7d824909efd09a267768b7
John Kiragu <mutuakiragu@gmail.com>
2021-07-06 12:29:16 +0300

    [feat] Builds the Autorization module of the application
    This builds a way to authorize the Safaricom API and access its
    endpoints

---
+++ src/Network/Payments/Mpesa/Auth.hs
@@ -0,0 +1,16 @@
+-- |
+-- Module: Network.Payments.Mpesa.Auth
+-- Copyright: (C) 2021 John Kiragu
+-- License: MIT (see LICENSE file)
+-- Maintainer: John Kiragu <mutuakiragu@gmail.com>
+-- Stability: experimental
+
+
+module Network.Payments.Mpesa.Auth where
+
+import Network.Wreq ()
+import Data.Aeson ()
+import Control.Lens ()
+import Network.Payments.Mpesa.Environment ()
+
+
---
+++ src/Network/Payments/Mpesa/Environment.hs
@@ -0,0 +1,1 @@
+ module Network.Payments.Mpesa.Environment where
