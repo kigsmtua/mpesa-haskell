bc529c19553dbb7f31abf11ed00c1ba866ee6de3
John Kiragu <mutuakiragu@gmail.com>
2021-07-06 17:07:54 +0300

    Finish up the basic Authentication Function of the Application How do we wrap that in A monad

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Network.Payments.Mpesa.Auth
-
 main :: IO ()
-main = putStrLn "someFunc"
+main = do
+    res <- fetchAccessToken "https://sandbox.safaricom.co.ke" "username" "password"
+    case res of
+        Left err -> putStrLn "bingo"
+        Right body ->
+            let token = aToken body
+            in putStrLn token
--- src/Network/Payments/Mpesa/Auth.hs
+++ src/Network/Payments/Mpesa/Auth.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- |
 -- Module: Network.Payments.Mpesa.Auth
 -- Copyright: (C) 2021 John Kiragu
@@ -5,23 +7,28 @@
 -- Maintainer: John Kiragu <mutuakiragu@gmail.com>
 -- Stability: experimental
 
-{-# LANGUAGE OverloadedStrings #-}
+module Network.Payments.Mpesa.Auth
+  ( ConsumerKey,
+    ConsumerSecret,
+    Seconds,
+    AccessToken (..),
+    AccessTokenError (..),
+    AccessTokenResult,
+    fetchAccessToken,
+  )
+where
 
-module Network.Payments.Mpesa.Auth where
-
-import Network.Wreq
-import Data.Aeson
-import Control.Lens
-
-import Control.Monad
 import Control.Exception
-import Data.Time.Clock
+import Control.Lens
+import Control.Monad
+import Data.Aeson
 import qualified Data.ByteString.Char8 as BS8
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Text as T
-import qualified Network.Wreq.Types as WTypes
+import Data.Time.Clock
 import qualified Network.HTTP.Client as HTTP
-
+import Network.Wreq
+import qualified Network.Wreq.Types as WTypes
 
 -- | Mpesa Consumer Key to generated for access to the Mpesa API
 type ConsumerKey = String
@@ -30,54 +37,49 @@ type ConsumerKey = String
 type ConsumerSecret = String
 
 -- | Number representing seconds of time.
-type Seconds = Integer
-
+type Seconds = String
 
 -- | Access token returned from OAuth.
 data AccessToken = AccessToken
-  {
-    aToken :: String
-  , aTokenExpiresIn :: Seconds
-  } deriving (Eq, Show)
+  { aToken :: String,
+    aTokenExpiresIn :: Seconds
+  }
+  deriving (Eq, Show)
 
-
-data AccessTokenError = AccessTokenHttpError HTTP.HttpException |
-                        AccessTokenStatusError Int |
-                        AccessTokenParseError String LBS.ByteString
+data AccessTokenError
+  = AccessTokenHttpError HTTP.HttpException
+  | AccessTokenStatusError Int
+  | AccessTokenParseError String LBS.ByteString
+  deriving (Show)
 
 instance FromJSON AccessToken where
   parseJSON (Object obj) =
-    AccessToken <$>
-    obj .: "access_token" <*>
-    obj .: "expiry_in"
+    AccessToken
+      <$> obj .: "access_token"
+      <*> obj .: "expires_in"
   parseJSON _ = mzero
 
-
--- |Either an access token or the error encountered while fetching it.
+-- | Either an access token or the error encountered while fetching it.
 type AccessTokenResult = Either AccessTokenError AccessToken
-
 
 -- | Fetch acess token from the Mpesa API
 fetchAccessToken :: String -> ConsumerKey -> ConsumerSecret -> IO AccessTokenResult
 fetchAccessToken url username password = do
   let usernameBS = BS8.pack username
       passwordBS = BS8.pack password
-      fullUrl = url ++ "/v1/oauth2/token"
-      options' = defaults & header "Accept" .~ ["application/json"] &
-                            auth ?~ basicAuth usernameBS passwordBS
-      contentType = "application/x-www-form-urlencoded"
-      content = "grant_type=client_credentials"
-      payload = WTypes.Raw contentType $ HTTP.RequestBodyBS content
-  responseOrErr <- (try $ postWith options' fullUrl payload) ::
-                   IO (Either HTTP.HttpException (Response LBS.ByteString))
+      fullUrl = url ++ "/oauth/v1/generate" -- Does this even begin to explain the gist of it all
+      options' = defaults & auth ?~ basicAuth usernameBS passwordBS & param "grant_type" .~ ["client_credentials"]
+  responseOrErr <-
+    (try $ getWith options' fullUrl) ::
+      IO (Either HTTP.HttpException (Response LBS.ByteString))
   case responseOrErr of
     Left err -> return $ Left $ AccessTokenHttpError err
     Right response ->
       let statusCode' = response ^. responseStatus . statusCode
-      in if statusCode' == 200 then
-        let responseText = response ^. responseBody
-        in return $ case eitherDecode responseText of
-             Left errMsg -> Left $ AccessTokenParseError errMsg responseText
-             Right result -> Right result
-      else
-        return $ Left $ AccessTokenStatusError statusCode'
+       in if statusCode' == 200
+            then
+              let responseText = response ^. responseBody
+               in return $ case eitherDecode responseText of
+                    Left errMsg -> Left $ AccessTokenParseError errMsg responseText
+                    Right result -> Right result
+            else return $ Left $ AccessTokenStatusError statusCode'
