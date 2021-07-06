e64b16162ac6b7790a25cd91e60a0514ad5b242a
John Kiragu <mutuakiragu@gmail.com>
2021-07-06 15:06:06 +0300

    Can we have an app to test with

--- src/Network/Payments/Mpesa/Auth.hs
+++ src/Network/Payments/Mpesa/Auth.hs
@@ -1,7 +1,7 @@
 -- |
 -- Module: Network.Payments.Mpesa.Auth
 -- Copyright: (C) 2021 John Kiragu
--- License: MIT (see LICENSE file)
+-- License: BSD3 (see LICENSE file)
 -- Maintainer: John Kiragu <mutuakiragu@gmail.com>
 -- Stability: experimental
 
